952a4e4e0e6b150cc6a1e75469dfed4ce0762b9d
smilerz <smilerz@gmail.com>
2024-02-16 07:22:53 -0600

    fixed error detection and logging added counts to progress when creating tandoor recipes

--- import_recipes.py
+++ import_recipes.py
@@ -1,7 +1,5 @@
-import json
 import os
 import shelve
-import time
 from uuid import NAMESPACE_OID, uuid3
 
 import configargparse
@@ -10,11 +8,9 @@ from tandoor import TandoorAPI
 
 
 def parse_args():
-    parser = configargparse.ArgParser(
-        config_file_parser_class=configargparse.ConfigparserConfigFileParser,
-        ignore_unknown_config_file_keys=True,
-        description='Add recipes converted from images by ChatGPT to Tandoor.'
-    )
+    parser = configargparse.ArgParser(config_file_parser_class=configargparse.ConfigparserConfigFileParser,
+                                      ignore_unknown_config_file_keys=True,
+                                      description='Add recipes converted from images by ChatGPT to Tandoor.')
     # application related switches
     parser.add_argument('-c', '--my-config', is_config_file=True, default='config.ini', help='Specify configuration file.')
     parser.add_argument('--input_folder', default='import', help='Folder with screenshots of recipes.')
@@ -26,9 +22,11 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+
 def reportError(msg, image, details):
     print(f"{image}: {msg}")
     print(details)
+
 
 args = parse_args()
 input_dir = args.input_folder
@@ -41,29 +39,32 @@ try:
     caches = shelve.open('caches.db', writeback=True)
     tandoor = str(uuid3(NAMESPACE_OID, args.tandoor_url))
     if tandoor not in caches:
-        caches[tandoor]={}
+        caches[tandoor] = {}
 
 except FileNotFoundError:
     caches = {}
 
+total = len(os.listdir(input_dir))
+count = 1
 for img_name in os.listdir(input_dir):
     if img_name in caches[tandoor]:
-        print(f"Skipping {img_name}, recipe already created.")
+        print(f"{count}/{total}: Skipping {img_name}, recipe already created.")
+        count += 1
         continue
 
     json_path = os.path.join(output_dir, os.path.splitext(img_name)[0] + ".png.json")
 
-    with open(os.path.join(input_dir, img_name),"rb") as img_file:
+    with open(os.path.join(input_dir, img_name), "rb") as img_file:
         image = (img_name, img_file.read(), None)
     with open(json_path) as f:
         json_data = {'data': f.read()}
-        json_data['data'] = json_data['data'].replace('```json','')
-        json_data['data'] = json_data['data'].replace('```','')
-        json_data['data'] = json_data['data'].replace(',\n',',')
-        json_data['data'] = json_data['data'].replace('{\n','{')
-        json_data['data'] = json_data['data'].replace('\n}','}')
+        json_data['data'] = json_data['data'].replace('```json', '')
+        json_data['data'] = json_data['data'].replace('```', '')
+        json_data['data'] = json_data['data'].replace(',\n', ',')
+        json_data['data'] = json_data['data'].replace('{\n', '{')
+        json_data['data'] = json_data['data'].replace('\n}', '}')
         json_data['data'] = json_data['data'].replace('[\n', '[')
-        json_data['data'] = json_data['data'].replace('"\n','"')
+        json_data['data'] = json_data['data'].replace('"\n', '"')
         json_data['data'] = json_data['data'].replace("'", r"\u0027")
         json_data['data'] = json_data['data'].replace(r"Park\u00ed\u00fur", "Pur Likor")
         json_data['data'] = json_data['data'].replace('"title":', '"name":')
@@ -71,16 +72,19 @@ for img_name in os.listdir(input_dir):
     success, recipe = api.get_recipe_from_json(json_data)
     if not success:
         reportError('Failed to convert json to a recipe.', img_name, recipe)
+        count += 1
         continue
     else:
-        recipe=recipe['recipe_json']
+        recipe = recipe['recipe_json']
     if not recipe['name']:
         reportError('Recipe has no name.', img_name, recipe)
+        count += 1
         continue
 
     success, image = api.create_file(recipe["name"], image)
     if not success:
         reportError('Failed to create image', img_name, image)
+        count += 1
         continue
 
     recipe["steps"][0]["file"] = image
@@ -88,16 +92,22 @@ for img_name in os.listdir(input_dir):
     try:
         success, response = api.create_recipe(recipe)
     except Exception as e:
-        pass
+        success = False
+        print(e)
     if not success:
         reportError('Failed to create recipe', img_name, response)
+        count += 1
         continue
+    else:
+        print(f"{count}/{total}: Succesfully created {recipe['name']} from {img_name}")
 
     if RECIPE_FOOD:
         success, response = api.create_food(response)
         if not success:
             reportError('Failed to create recipe as food', img_name, response)
+            count += 1
             continue
 
     caches[tandoor][img_name] = {'recipe': recipe['name']}
     caches.sync()
+    count += 1
--- tandoor.py
+++ tandoor.py
@@ -1,4 +1,3 @@
-import json
 import mimetypes
 
 import requests
@@ -6,56 +5,42 @@ from requests_toolbelt import MultipartEncoder
 
 
 class TandoorAPI:
+
     def __init__(self, url, token, **kwargs):
         self.token = token
         self.url = url
-        self.headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {self.token}'
-        }
+        self.headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {self.token}'}
         self.session = requests.Session()
-        self.success = True
 
     def get_recipe_from_json(self, json):
         url = self.url + "recipe-from-source/"
         response = self.session.post(url, json=json, headers=self.headers)
         if response.status_code != 200:
-            self.success = False
-        return self.success, response.json()
+            return False, response.json()
+        return True, response.json()
 
     def create_recipe(self, recipe):
         url = self.url + "recipe/"
         response = self.session.post(url, json=recipe, headers=self.headers)
         if response.status_code != 201:
-            self.success = False
-        return self.success, response.json()
+            return False, response.json()
+        return True, response.json()
 
     def create_file(self, name, image):
         url = self.url + "user-file/"
         headers = self.headers.copy()
 
-        m = MultipartEncoder(fields={
-            'name': name,
-            'file': (image[0], image[1], mimetypes.guess_type(image[0])[0])
-        })
-        headers['Content-Type'] =  m.content_type
+        m = MultipartEncoder(fields={'name': name, 'file': (image[0], image[1], mimetypes.guess_type(image[0])[0])})
+        headers['Content-Type'] = m.content_type
         response = self.session.post(url, data=m, headers=headers)
         if response.status_code != 201:
-            self.success = False
-        return self.success, response.json()
+            return False, response.json()
+        return True, response.json()
 
     def create_food(self, recipe):
         url = self.url + "food/"
-        food = {
-            'name': recipe['name'].lower(),
-            'recipe': {
-                'id': recipe['id'],
-                'name': recipe['name']
-            }
-        }
+        food = {'name': recipe['name'].lower(), 'recipe': {'id': recipe['id'], 'name': recipe['name']}}
         response = self.session.post(url, json=food, headers=self.headers)
         if response.status_code != 201:
-            self.success = False
-        return self.success, response.json()
-
-
+            return False, response.json()
+        return True, response.json()
